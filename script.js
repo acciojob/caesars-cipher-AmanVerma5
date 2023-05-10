83f338227a2e9e59b1ff7b7938ae73ed622585d2
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-05-10 18:59:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,8 +13,16 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
-  return ;//return decodedArr
+	for(let i=0;i<encodedStr.length;i++){
+		let c=encodedStr.charCodeAt(i);
+		if(c>=65 && c<=90){
+			c=((c-65+13)%26)+65;
+			decodedArr.push(String.fromCharCode(c));
+		}else{
+			decodedArr.push(String.fromCharCode(c));
+		}
+	}
+  return decodedArr;//return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
